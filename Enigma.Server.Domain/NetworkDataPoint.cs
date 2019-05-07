cb3fc4bf403895a8b50bc767778842e040790eb7
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2019-05-07 19:55:53 -0400

    Added event listener for a new TCP connection

---
+++ Enigma.Server.Domain/NetworkDataPoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enigma.Server.Domain
+{
+    public class NetworkDataPoint
+    {
+        private object _node;
+        public object Node
+        {
+            get { return _node; }
+            set
+            {
+                // This should really never be null.
+                if (value != null)
+                {
+                    _nodeType = value.GetType();
+                }
+
+                _node = value;
+            }
+        }
+
+        private Type _nodeType;
+
+        public bool IsOfType<T>()
+        {
+            return _nodeType == typeof(T) || _nodeType.IsSubclassOf(typeof(T));
+        }
+
+        public T GetAsT<T>()
+        {
+            return (T) _node;
+        }
+    }
+}
---
+++ Enigma.Server.Networking/ConnectionHandlers/ConnectionInitializationListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Enigma.Server.Domain;
+
+namespace Enigma.Server.Networking.ConnectionHandlers
+{
+    public class ConnectionInitializationListener
+    {
+        private readonly TcpListener _tcpListener;
+        public event EventHandler<Socket> NewSocketEvent;
+
+        public ConnectionInitializationListener()
+        {
+            // Use any for the local address vs calling DNS GetHost Name
+            // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?view=netframework-4.8
+            _tcpListener = new TcpListener(IPAddress.Any, StartupInfo.PortNum);
+            new Thread(ListenOnSeperateThread).Start();
+        }
+
+        private void ListenOnSeperateThread()
+        {
+            var socket = _tcpListener.AcceptSocket();
+            // Note: The subscriptions to events will be ran on this listener thread
+            // Hypothetically if something were to take a really long time we could be not listening for some time
+            // And, this also allows a subscriber to cause race conditions
+            NewSocketEvent?.Invoke(this, socket);
+        }
+    }
+}
