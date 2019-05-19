0021cdf77960acaf9d790bde999733eef306e132
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2019-05-19 15:16:24 -0400

    TCP chunked read/write first unit test

--- Enigma.Server.Networking/ConnectionHandlers/TcpConnectionHandler.cs
+++ Enigma.Server.Networking/ConnectionHandlers/TcpConnectionHandler.cs
@@ -9,18 +9,30 @@ namespace Enigma.Server.Networking.ConnectionHandlers
     public class TcpConnectionHandler : IConnectionHandler
     {
         private readonly Socket _socket;
+        public Stack<string> Messages { get; }
         public TcpConnectionHandler(Socket socket)
         {
             _socket = socket;
             new Thread(ListenOverSocket).Start();
+            Messages = new Stack<string>();
         }
 
         private void ListenOverSocket()
         {
             while (true)
             {
-                // some code to read the size of the chunk, then sync read it until it's finished.
+                var messageSize = GetMessageSize();
+                var messageArray = new byte[messageSize];
+                _socket.Receive(messageArray);
+                Messages.Push(Encoding.UTF8.GetString(messageArray));
             }
+        }
+
+        private int GetMessageSize()
+        {
+            var bytes = new byte[4];
+            var size = _socket.Receive(bytes);
+            return BitConverter.ToInt32(bytes);
         }
     }
 }
---
+++ EnigmaServer.Networking.UnitTests/TcpMessagingTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Enigma.Server.Domain;
+using Enigma.Server.Networking.ConnectionHandlers;
+using EnigmaServer.Networking.UnitTests.HelperClasses;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace EnigmaServer.Networking.UnitTests
+{
+    [TestFixture]
+    public class TcpMessagingTests
+    {
+        private ConnectionInitializationListener _connectionInitializationListener;
+
+        public TcpMessagingTests()
+        {
+            StartupInfo.PortNum = 54232;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            _connectionInitializationListener = new ConnectionInitializationListener();
+        }
+
+
+        [Test]
+        public void WriteMessageOverTcpPort()
+        {
+            var obj = new TestMessage
+            {
+                Test = 42
+            };
+
+            var jsonObj = JsonConvert.SerializeObject(obj);
+            var jsonAsBytes = Encoding.UTF8.GetBytes(jsonObj);
+            var bytes = new List<byte>();
+            bytes.AddRange(BitConverter.GetBytes(jsonAsBytes.Length));
+            bytes.AddRange(jsonAsBytes);
+
+            var tcpClient = new TcpClient();
+            var callBackContainer = new CallBackContainer<Socket>();
+
+            _connectionInitializationListener.NewSocketEvent += callBackContainer.CallBackMethod;
+
+            tcpClient.Connect(Dns.GetHostName(), StartupInfo.PortNum);
+            
+            callBackContainer.WaitForCallBack(1000);
+
+            var tcpConnectionHandler = new TcpConnectionHandler(callBackContainer.CallBackObject);
+
+            tcpClient.Client.Send(bytes.ToArray());
+
+            Assert.AreEqual(jsonObj, tcpConnectionHandler.Messages.Pop());
+        }
+
+        private class TestMessage
+        {
+            public int Test { get; set; }
+        }
+    }
+
+}
